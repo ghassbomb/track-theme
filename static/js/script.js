1940e8ca180daabaa85e1b60b19368a49699e284
Ghassan Shahzad <ghass.shahzad@gmail.com>
2024-02-04 21:25:59 +0500

    fix: Fixed the 'back-to-top' button and put the JS code for it in the base template instead of a dedicated JS file.

--- static/js/script.js
+++
@@ -1,7 +0,0 @@
-let mybutton = document.getElementById("myBtn");
-window.onscroll = function() {scrollFunction()};
-
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
